159a6e895aa7bd9ba2d2a72cac6c6b8c7976cc6c
Schyler Hosler <schylerhosler@gmail.com>
2016-08-14 20:19:47 -0400

    Initial commit:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,52 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  # get '/' do
+  #   erb :root
+  # end
+
+  get '/posts/new' do
+    erb :new
   end
+
+  post '/posts' do
+    #binding.pry
+    #Model.create(some_attribute: params[:some_attribute])
+    @post = Post.create(name: params[:name], content: params[:content])#[:post]) #Create: Model.create
+    #erb :index
+    redirect '/posts'
+  end
+
+  get '/posts' do
+    #binding.pry
+    #@models = Model.all
+    @posts = Post.all #Read: Model.all/Model.find(id_number)
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    # @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :deleted
+  end
+
 end
---
+++ app/models/post.rb
@@ -0,0 +1,21 @@
+class Post < ActiveRecord::Base
+
+  # attr_accessor :name, :content, :id
+  #
+  # ALL = []
+  #
+  # def initialize(args)
+  #   @name = args[:name]
+  #   @content = args[:content]
+  #   ALL << self
+  # end
+  #
+  # def self.all
+  #   ALL
+  # end
+  #
+  # def self.clear
+  #   ALL.clear
+  # end
+
+end
